26ab686fe2594125b33bbc810b39bb4da501ad3d
Ilya Manyahin <i.manyahin@gmail.com>
2015-05-08 20:18:30 +0300

    Make display events dinamically

--- urbanist.js
+++ urbanist.js
@@ -1,13 +1,29 @@
 Events = new Mongo.Collection("events");
 
+if (Meteor.isClient) {
+
+	Template.allEvents.helpers({
+		events: function() {
+			return Events.find({});
+		}
+	});
+
+	Template.oneEvent.helpers({
+		statusIs : function(status) {
+			return this.status === status;
+		}
+	});
+
+}
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
   	// If collection is empty then fill it from the json file. 
     if (Events.find().count() === 0) {
-			var events_data = JSON.parse(Assets.getText('events.json'));
-			events_data.forEach(function(obj) {
-				Events.insert(obj);
-			})
+		var events_data = JSON.parse(Assets.getText('events.json'));
+		events_data.forEach(function(obj) {
+			Events.insert(obj);
+		})
     }
   });
 }
